64b164cb1672fc5a9e7fb9565be12f5779ce4e16
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2018-08-15 07:19:53 +0300

    nit

--- core/tls.cc
+++ core/tls.cc
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-#define TLS_HANDLE(tls, rc, fd) \
+#define TLS_HANDLE_ERR(tls, rc, fd) \
 { \
 	int err = wolfSSL_get_error((tls), (rc)); \
 	if (err == WOLFSSL_ERROR_WANT_READ) \
@@ -117,7 +117,7 @@ void TLS::tlsConnect(S6U::SocketAddress *addr, StreamBuffer *buf, bool useEarlyD
 	else
 		rc = wolfSSL_connect(readTLS);
 	if (rc != SSL_SUCCESS)
-		TLS_HANDLE(readTLS, rc, rfd);
+		TLS_HANDLE_ERR(readTLS, rc, rfd);
 	
 	if (useEarlyData)
 		buf->unuse(earlyDataWritten);
@@ -128,7 +128,7 @@ void TLS::tlsAccept(StreamBuffer *buf)
 	int earlyDataRead = 0;
 	int rc = wolfSSL_read_early_data(readTLS, buf->getTail(), buf->usedSize(), &earlyDataRead);
 	if (rc < 0)
-		TLS_HANDLE(writeTLS, rc, rfd);
+		TLS_HANDLE_ERR(writeTLS, rc, rfd);
 	
 	buf->use(earlyDataRead);
 }
@@ -137,7 +137,7 @@ size_t TLS::tlsWrite(StreamBuffer *buf)
 {
 	int bytes = wolfSSL_write(writeTLS, buf->getHead(), buf->usedSize());
 	if (bytes < 0)
-		TLS_HANDLE(writeTLS, bytes, wfd);
+		TLS_HANDLE_ERR(writeTLS, bytes, wfd);
 	
 	buf->unuse(bytes);
 	return bytes;
@@ -147,7 +147,7 @@ size_t TLS::tlsRead(StreamBuffer *buf)
 {
 	int bytes = wolfSSL_read(readTLS, buf->getTail(), buf->availSize());
 	if (bytes < 0)
-		TLS_HANDLE(readTLS, bytes, rfd);
+		TLS_HANDLE_ERR(readTLS, bytes, rfd);
 	
 	buf->use(bytes);
 	return bytes;
