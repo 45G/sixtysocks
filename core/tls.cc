d25625d6184cfc883b3d4df609ab02f1ada12c79
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2019-06-06 06:11:53 +0300

    todo

--- core/tls.cc
+++ core/tls.cc
@@ -218,6 +218,7 @@ PRInt32 PR_CALLBACK TLS::dSend(PRFileDesc *fd, const void *buf, PRInt32 amount, 
 		rc = send(tls->writeFD, buf, amount, flags);
 	}
 
+	//TODO: proper solution; this is a HACK
 	int err = errno == EINPROGRESS ? EWOULDBLOCK : errno; 
 	if (rc < 0)
 		_MD_unix_map_send_error(err);
