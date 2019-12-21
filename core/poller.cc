aab5e964b52d2eab120a9008b9a29e3c05ada4e6
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2019-12-21 06:44:17 +0200

    oopsie

--- core/poller.cc
+++ core/poller.cc
@@ -78,7 +78,7 @@ void Poller::add(intrusive_ptr<Reactor> reactor, int fd, uint32_t events)
 
 void Poller::remove(int fd, bool force)
 {
-	if ((!fdEntries[fd].registered || fd < 0) && !force)
+	if ((fd < 0 || !fdEntries[fd].registered) && !force)
 		return;
 
 	int rc = epoll_ctl(epollFD, EPOLL_CTL_DEL, fd, nullptr);
